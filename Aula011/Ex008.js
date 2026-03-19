18aac85b6bb33ecff5f61a04efc06bfadcc8a51e
Douglas20033 <camposdouglas01@gmail.com>
2026-03-19 13:51:04 -0300

    Atualização de JS - Condições

---
+++ Aula011/Ex008.js
@@ -0,0 +1,8 @@
+var vel = 55.9
+console.log(`A velocidade do seu carro é ${vel}KM/h `)
+if (vel >=60.00) {
+    console.log('Você ultrapassou a velocidade permitida e foi multado')
+}
+
+console.log('Utilize sempre o sinto de segurança')
+
